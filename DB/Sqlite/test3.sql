27070a0a1bcfea2f63dc4b2c2901692ce563708a
Piyoshi <usususus3104@gmail.com>
2013-10-30 02:14:15 +0900

    Railsの基礎学習

---
+++ DB/Sqlite/test3.sql
@@ -0,0 +1,13 @@
+insert into games (user_id, score) values (1, 200);
+insert into games (user_id, score) values (1, 300);
+insert into games (user_id, score) values (1, 20);
+insert into games (user_id, score) values (1, 220);
+insert into games (user_id, score) values (2, 120);
+insert into games (user_id, score) values (2, 50);
+insert into games (user_id, score) values (2, 40);
+insert into games (user_id, score) values (3, 40);
+insert into games (user_id, score) values (3, 120);
+insert into games (user_id, score) values (3, 140);
+insert into games (user_id, score) values (4, 140);
+insert into games (user_id, score) values (4, 10);
+insert into games (user_id, score) values (4, 100);
