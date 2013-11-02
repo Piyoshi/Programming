82405c8daeeb7314e48810cf81dfc8e89209aafa
Piyoshi <usususus3104@gmail.com>
2013-11-02 18:53:01 +0900

    miniSNSを作った

---
+++ RubyProgramming/RailsProgramming/miniSNS/app/models/message.rb
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+class Message < ActiveRecord::Base
+  attr_accessible :content, :member_id
+
+  belongs_to :member
+  has_many :comment
+end
---
+++ RubyProgramming/RailsProgramming/miniSNS/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniSNS::Application.config.secret_token = '4e821b981d552d5944226b86e967c4c59b766c16d6f9e4ead891ca1282b1570489e3a3141bd6b44e9a2323ef738e7319c5a945488c8d4d78b297467c52feb0ea'
