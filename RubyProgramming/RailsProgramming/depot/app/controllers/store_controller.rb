1370c6c3e55f8fc25efbd9c868045cc690585b0b
Piyoshi <usususus3104@gmail.com>
2013-12-22 19:48:24 +0900

    Rails Depot TaskE,F Done

--- RubyProgramming/RailsProgramming/depot/app/controllers/store_controller.rb
+++ RubyProgramming/RailsProgramming/depot/app/controllers/store_controller.rb
@@ -1,6 +1,10 @@
 class StoreController < ApplicationController
   def index
+    if session[:counter].nil? then; session[:counter] = 0; end
+    session[:counter] += 1
+	@count = session[:counter]
     @products = Product.order(:title)
+	@cart = current_cart
 	@date = Date.new
   end
 end
